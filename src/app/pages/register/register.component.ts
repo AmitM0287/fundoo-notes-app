486726b848a405052c86dd53b869d32153b2cd0c
AmitM0287 <amitmanna0287@gmail.com>
2021-06-11 22:48:48 +0530

    feat: validators added

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,17 +10,32 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  // Declare varibles
+  firstName = new FormControl('');
+  lastName = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('');
+  password = new FormControl('');
+  confirm = new FormControl('');
+  
+  // Validate email
+  validateEmail() {
+    if (this.email.hasError('required')) {
+      return 'Email is required';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  // Register
+  register() {
+    console.log(this.firstName.value)
   }
 
   // Login
   login() {
-    
-  }
-  
-  // Register
-  register() {
-
+    console.log(this.confirm.value)
   }
 
 }
