fcaa2d2da3b7b97b7ea1d30de81edce77b29d6bc
AmitM0287 <amitmanna0287@gmail.com>
2021-06-13 13:04:42 +0530

    feat: send request & data to backend added

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,19 +8,42 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  // Constructor
+  constructor(private service: UserService) { 
 
-  constructor() { }
+  }
 
-  ngOnInit(): void { }
+  // ngOnInit
+  ngOnInit(): void { 
+
+  }
+
+  // Set hide to true
+  hide = true;
 
   // Declare varibles
-  firstName = new FormControl('');
-  lastName = new FormControl('');
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  username = new FormControl('');
-  password = new FormControl('');
-  confirm = new FormControl('');
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
   
+  // Validate first name
+  validateFirstName() {
+    if (this.firstName.hasError('required')) {
+      return 'First name is required';
+    }
+    return '';
+  }
+
+  // Validate first name
+  validateLastName() {
+    if (this.lastName.hasError('required')) {
+      return 'Last name is required';
+    }
+    return '';
+  }
+
   // Validate email
   validateEmail() {
     if (this.email.hasError('required')) {
@@ -28,14 +52,40 @@ export class RegisterComponent implements OnInit {
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
+  // Validate username
+  validateUsername() {
+    if (this.username.hasError('required')) {
+      return 'Username is required';
+    }
+    return '';
+  }
+
+  // Validate password
+  validatePassword() {
+    if (this.password.hasError('required')) {
+      return 'Password is required';
+    }
+    return '';
+  }
+
   // Register
   register() {
-    console.log(this.firstName.value)
+    this.service.registerUser({
+        "first_name": this.firstName.value,
+        "last_name": this.lastName.value,
+        "email": this.email.value,
+        "username": this.username.value,
+        "password": this.password.value}).subscribe((response) => {
+      console.log(response);
+      alert('Registration successful!');
+    }, (error) => {
+      console.log(error);
+    });
   }
 
   // Login
   login() {
-    console.log(this.confirm.value)
+    
   }
 
 }
