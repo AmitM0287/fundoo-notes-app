0bb58b544dd14ac47a956e51e2408f368085c16a
AmitM0287 <amitmanna0287@gmail.com>
2021-06-14 14:03:09 +0530

    feat: user login api call added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,11 +9,16 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   // URL
-  api_url = 'http://127.0.0.1:8000/user/register/';
+  register_url = 'http://127.0.0.1:8000/user/register/';
+  login_url = 'http://127.0.0.1:8000/user/login/';
 
   // Register user
   registerUser(userData: any) {
-    return this.http.post(this.api_url, userData);
+    return this.http.post(this.register_url, userData);
   }
 
+  // Login user
+  loginUser(userData: any) {
+    return this.http.post(this.login_url, userData);
+  }
 }
