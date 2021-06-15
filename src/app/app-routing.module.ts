d8153d2189dda7bcb504abb8e4170df23fe11a9c
AmitM0287 <amitmanna0287@gmail.com>
2021-06-15 09:25:17 +0530

    feat: reset password component & forgot password component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'forgot/password', component: ForgotPasswordComponent },
+  { path: 'reset/password', component: ResetPasswordComponent },
 ];
 
 @NgModule({
