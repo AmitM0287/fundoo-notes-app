202a03936f416d7ac1d2eb02baa6a574b5133fb8
AmitM0287 <amitmanna0287@gmail.com>
2021-06-15 19:40:21 +0530

    refactor: configure baseURL & concatinate with that to avoid DRY principle

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,30 +9,24 @@ export class UserService {
   // Constructor
   constructor(private http: HttpClient) { }
 
-  // URL
-  registerURL = 'http://127.0.0.1:8000/user/register/';
-  loginURL = 'http://127.0.0.1:8000/user/login/';
-  resetPasswordURL = 'http://127.0.0.1:8000/user/reset/password/';
-  forgotPasswordURL = 'http://127.0.0.1:8000/user/forgot/password/';
-
   // Register user
   registerUser(userData: any) {
-    return this.http.post(this.registerURL, userData);
+    return this.http.post(environment.baseURL.concat('register/'), userData);
   }
 
   // Login user
   loginUser(userData: any) {
-    return this.http.post(this.loginURL, userData);
+    return this.http.post(environment.baseURL.concat('login/'), userData);
   }
 
   // Reset password
   resetPassword(userData: any) {
-    return this.http.put(this.resetPasswordURL, userData);
+    return this.http.put(environment.baseURL.concat('reset/password/'), userData);
   }
 
   // Forgot password
   forgotPassword(userData: any) {
-    return this.http.post(this.forgotPasswordURL, userData);
+    return this.http.post(environment.baseURL.concat('forgot/password/'), userData);
   }
 
 }
