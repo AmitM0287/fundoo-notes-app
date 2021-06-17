f895d426403e96040d6f2afd732cbc6abbddd4e1
AmitM0287 <amitmanna0287@gmail.com>
2021-06-17 19:51:15 +0530

    refactor: create notes component updated

---
+++ src/app/components/create-notes/create-notes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-notes',
+  templateUrl: './create-notes.component.html',
+  styleUrls: ['./create-notes.component.scss']
+})
+export class CreateNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  panelOpenState = false;
+
+}
