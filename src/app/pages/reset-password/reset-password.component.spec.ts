fa8d4a3c1bb71542fcc42f80e07bc305f0d9e89b
AmitM0287 <amitmanna0287@gmail.com>
2021-06-19 09:09:33 +0530

    test: test cases added

--- src/app/pages/reset-password/reset-password.component.spec.ts
+++ src/app/pages/reset-password/reset-password.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResetPasswordComponent } from './reset-password.component';
+import { TitleComponent } from 'src/app/components/title/title.component';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
@@ -11,7 +13,7 @@ describe('ResetPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ],
+      declarations: [ ResetPasswordComponent, TitleComponent ],
       imports: [
         HttpClientTestingModule,
         MatSnackBarModule,
@@ -28,5 +30,16 @@ describe('ResetPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have <h1> with "Reset your Fundoo Account password"', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const h1 = bannerElement.querySelector('h1')!;
+    expect(h1.textContent).toEqual('Reset your Fundoo Account password');
+  });
+
+  it('should have <app-title> with "FundooNotes', ()=> {
+    const de = fixture.debugElement.query(By.css('app-title'));
+    expect(de.nativeElement.textContent).toEqual('FundooNotes');
   });
 });
