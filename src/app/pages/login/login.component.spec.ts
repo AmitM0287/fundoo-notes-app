d6f2d6a4bf5599372b8b78b46b4d45254014d598
AmitM0287 <amitmanna0287@gmail.com>
2021-06-19 09:07:42 +0530

    test: test cases added

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { TitleComponent } from 'src/app/components/title/title.component';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,7 +14,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
+      declarations: [ LoginComponent, TitleComponent ],
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
@@ -30,5 +32,16 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have <h1> with "Login with your Fundoo Account"', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const h1 = bannerElement.querySelector('h1')!;
+    expect(h1.textContent).toEqual('Login with your Fundoo Account');
+  });
+
+  it('should have <app-title> with "FundooNotes', ()=> {
+    const de = fixture.debugElement.query(By.css('app-title'));
+    expect(de.nativeElement.textContent).toEqual('FundooNotes');
   });
 });
