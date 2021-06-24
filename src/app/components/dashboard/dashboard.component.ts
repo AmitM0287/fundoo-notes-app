bc7f2408dd18328421e3e60cbe8cdb420e6ca61e
AmitM0287 <amitmanna0287@gmail.com>
2021-06-24 09:29:01 +0530

    refactor: dashboard component added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  // Set isExpanded to false
+  isExpanded: boolean = false;
+
+  // Getting username from local storage
+  username = localStorage.getItem('username')?.toUpperCase()
+
+  // Sign Out
+  signOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    this.router.navigateByUrl('login');
+  }
+
+  // notes
+  notes() {
+    this.router.navigateByUrl('dashboard/notes');
+  }
+
+  // reminders
+  reminders() {
+
+  }
+
+  // labels
+  labels() {
+
+  }
+
+  // archive
+  archive() {
+
+  }
+
+  // trash
+  trash() {
+
   }
   
 }
