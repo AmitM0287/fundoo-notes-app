f79b4f61af48aeca1fe702e3b4401e906b9ae386
AmitM0287 <amitmanna0287@gmail.com>
2021-06-24 09:25:40 +0530

    feat: children routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNotesComponent } from './components/create-notes/create-notes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -11,7 +12,10 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, 
+    children: [
+      { path: 'notes', component: CreateNotesComponent }, 
+    ]},
 ];
 
 @NgModule({
