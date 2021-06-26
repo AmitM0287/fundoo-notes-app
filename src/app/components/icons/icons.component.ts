dabb9ebb653d2d6ee88c10ffe89de675e063607a
AmitM0287 <amitmanna0287@gmail.com>
2021-06-26 18:01:14 +0530

    refactor: icons component updated

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NotesService } from 'src/app/services/notes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-icons',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
+  @Input() noteId: any;
 
-  constructor() { }
+  // Constructor
+  constructor(private service: NotesService, private snackBar: MatSnackBar, private interaction: InteractionService) { }
 
+  // ngOnInit
   ngOnInit(): void {
   }
 
+  // archiveNote
+  archiveNote() {
+    this.service.archiveNotes({
+      "note_id": this.noteId
+    }).subscribe((response) => {
+      this.snackBar.open('Note archived successfully!', '', {duration: 2000});
+      this.interaction.sendNewContent('Archive note');
+    }, (error) => {
+      this.snackBar.open('Note archived failed!', '', {duration: 2000});
+    });
+  }
 }
