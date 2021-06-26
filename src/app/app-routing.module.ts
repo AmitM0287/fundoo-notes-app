b6356796f7bfaedc42ce792f5cb0ceaba7f344be
AmitM0287 <amitmanna0287@gmail.com>
2021-06-26 18:04:13 +0530

    feat: archive, trash child routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateNotesComponent } from './components/create-notes/create-notes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IsArchivedComponent } from './components/is-archived/is-archived.component';
+import { IsTrashedComponent } from './components/is-trashed/is-trashed.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -14,7 +16,10 @@ const routes: Routes = [
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'dashboard', component: DashboardComponent, 
     children: [
+      { path: '', component: CreateNotesComponent },
       { path: 'notes', component: CreateNotesComponent }, 
+      { path: 'trash', component: IsTrashedComponent },
+      { path: 'archive', component: IsArchivedComponent },
     ]},
 ];
 
