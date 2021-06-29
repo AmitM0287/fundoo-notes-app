51484d3f103e3c299122b2c67659495e458ef228
AmitM0287 <amitmanna0287@gmail.com>
2021-06-29 16:36:19 +0530

    refactor: update component updated.

--- src/app/components/update-notes/update-notes.component.ts
+++ src/app/components/update-notes/update-notes.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NotesService } from 'src/app/services/notes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-update-notes',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-notes.component.scss']
 })
 export class UpdateNotesComponent implements OnInit {
+  // Constructor
+  constructor(@Inject(MAT_DIALOG_DATA) public dialogData: any, private service: NotesService, private snackBar: MatSnackBar, private interaction: InteractionService) { }
 
-  constructor() { }
-
+  // ngOnInit
   ngOnInit(): void {
+  }
+
+  // Close
+  close() {
+    this.service.updateNotes({
+      "id": this.dialogData.noteData.id,
+      "title": this.dialogData.noteData.title,
+      "description": this.dialogData.noteData.description
+    }).subscribe((response) => {
+      this.snackBar.open('Note updated successfully!', '', {duration: 2000});
+      this.interaction.sendContent('Note updated');
+    }, (error) => {
+      this.snackBar.open('Note update failed!', '', {duration: 2000});
+    });
   }
 
 }
