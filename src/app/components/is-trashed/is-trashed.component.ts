3b340adcd8b6e510f293d7a8a0cebb455790d975
AmitM0287 <amitmanna0287@gmail.com>
2021-06-29 10:49:02 +0530

    refactor: is-trashed component updated

--- src/app/components/is-trashed/is-trashed.component.ts
+++ src/app/components/is-trashed/is-trashed.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotesService } from 'src/app/services/notes.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-is-trashed',
@@ -9,10 +10,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class IsTrashedComponent implements OnInit {
 
-  constructor(private service: NotesService, private snackBar: MatSnackBar,) { }
+  constructor(private service: NotesService, private snackBar: MatSnackBar, private interaction: InteractionService) { }
 
   ngOnInit(): void {
-    this.trashNotes()
+    this.trashNotes();
+    this.interaction.removeNote$.subscribe(() => {
+      this.trashNotes();
+    });
+    this.interaction.deleteNote$.subscribe(() => {
+      this.trashNotes();
+    });
   }
 
   // Getting notes
@@ -23,11 +30,41 @@ export class IsTrashedComponent implements OnInit {
     this.notes = response;
     this.notesArray = this.notes.data.notes_list.reverse();
     // Getting archive notes successfully
-    this.snackBar.open("Getting trash notes successfully.", '', {duration: 2000});
+    // this.snackBar.open("Getting trash notes successfully.", '', {duration: 2000});
     },
     (error) => {
       console.log(error);
       this.snackBar.open("You don't have any trash notes.", '', {duration: 2000});
+    });
+  }
+
+  // Get note id
+  noteId: any;
+  getNoteId(noteId: any) {
+    this.noteId = noteId;
+  }
+
+  // Restore Note
+  restoreNote() {
+    this.service.restoreNotes({
+      "note_id": this.noteId
+    }).subscribe((response) => {
+      this.snackBar.open('Note restored successfully!', '', {duration: 2000});
+      this.interaction.removeContent('Restore note');
+    }, (error) => {
+      this.snackBar.open('Note restored failed!', '', {duration: 2000});
+    });
+  }
+
+  // Delete Note
+  deleteNote() {
+    this.service.deleteNotes({
+      "note_id": this.noteId
+    }).subscribe((response) => {
+      this.snackBar.open('Note restored successfully!', '', {duration: 2000});
+      this.interaction.deleteContent('Delete note');
+    }, (error) => {
+      this.snackBar.open('Note deletion failed!', '', {duration: 2000});
     });
   }
 
