1d827992f523e272832ce690ea4b7ed7bc174e61
AmitM0287 <amitmanna0287@gmail.com>
2021-06-29 10:47:58 +0530

    refactor: is-archived component updated

--- src/app/components/is-archived/is-archived.component.ts
+++ src/app/components/is-archived/is-archived.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotesService } from 'src/app/services/notes.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-is-archived',
@@ -9,11 +10,17 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class IsArchivedComponent implements OnInit {
   // Constructor
-  constructor(private service: NotesService, private snackBar: MatSnackBar,) { }
+  constructor(private service: NotesService, private snackBar: MatSnackBar, private interaction: InteractionService) { }
 
   // ngOnInit
   ngOnInit(): void {
     this.archiveNotes();
+    this.interaction.createNote$.subscribe(() => {
+      this.archiveNotes();
+    });
+    this.interaction.removeNote$.subscribe(() => {
+      this.archiveNotes();
+    });
   }
 
   // Getting notes
@@ -24,11 +31,41 @@ export class IsArchivedComponent implements OnInit {
     this.notes = response;
     this.notesArray = this.notes.data.notes_list.reverse();
     // Getting archive notes successfully
-    this.snackBar.open("Getting archive notes successfully.", '', {duration: 2000});
+    // this.snackBar.open("Getting archive notes successfully.", '', {duration: 2000});
     },
     (error) => {
       console.log(error);
       this.snackBar.open("You don't have any archive notes.", '', {duration: 2000});
+    });
+  }
+
+  // Get note id
+  noteId: any;
+  getNoteId(noteId: any) {
+    this.noteId = noteId;
+  }
+
+  // unArchiveNote
+  unArchiveNote() {
+    this.service.unArchiveNotes({
+      "note_id": this.noteId
+    }).subscribe((response) => {
+      this.snackBar.open('Note UnArchived successfully!', '', {duration: 2000});
+      this.interaction.removeContent('unArchive note');
+    }, (error) => {
+      this.snackBar.open('Note UnArchived failed!', '', {duration: 2000});
+    });
+  }
+
+  // trashArchiveNote
+  trashArchiveNote() {
+    this.service.trashNotes({
+      "note_id": this.noteId
+    }).subscribe((response) => {
+      this.snackBar.open('Note trashed successfully!', '', {duration: 2000});
+      this.interaction.removeContent('Trash note');
+    }, (error) => {
+      this.snackBar.open('Note trashed failed!', '', {duration: 2000});
     });
   }
  
